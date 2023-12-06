412e6a734e45fbcb42e16f9cb692718af02ebbd8
Ondřej Mottl <60345393+OndrejMottl@users.noreply.github.com>
2023-12-06 09:45:41 +0100

    Data overview: visualise the data in terms of plot size (#11)

--- R/02_Main_analyses/Plot_data/03_BIEN_review.R
+++ R/02_Main_analyses/Plot_data/03_BIEN_review.R
@@ -58,28 +58,76 @@ data_bien %>%
   ) +
   ggplot2::geom_point(alpha = 0.1) +
   ggplot2::geom_density2d() +
-  ggplot2::coord_quickmap()
-
+  ggplot2::coord_quickmap() +
+  ggplot2::theme(
+    text = ggplot2::element_text(
+      size = 25
+    )
+  ) +
+  ggplot2::labs(
+    title = "BIEN plots with coordinates and plot size",
+    x = "Longitude",
+    y = "Latitude",
+    caption = paste(
+      "N datasets = ",
+      data_bien %>%
+        tidyr::drop_na(longitude, latitude, plot_area_ha) %>%
+        nrow()
+    )
+  )
 
 # Get the most common size
 bien_most_common_size <-
   data_bien %>%
+  tidyr::drop_na(longitude, latitude, plot_area_ha) %>%
   dplyr::distinct(
     datasource, methodology_reference,
-    datasource_id, plot_name, plot_area_ha
+    datasource_id, plot_name, plot_area_ha,
+    .keep_all = TRUE
   ) %>%
-  tidyr::drop_na(plot_area_ha) %>%
-  dplyr::group_by(plot_area_ha) %>%
+  dplyr::mutate(
+    continent = dplyr::case_when(
+      .default = "Other",
+      latitude > 18 & longitude < -40 ~ "North America",
+      latitude < 18 & longitude < -40 ~ "Latin America"
+    )
+  ) %>%
+  dplyr::group_by(continent, plot_area_ha) %>%
   dplyr::count() %>%
   dplyr::arrange(desc(n)) %>%
-  purrr::chuck("plot_area_ha", 1)
+  dplyr::group_by(continent) %>%
+  dplyr::slice(1) %>%
+  dplyr::filter(
+    continent %in% c("North America", "Latin America")
+  ) %>%
+  purrr::chuck("plot_area_ha") %>%
+  rlang::set_names(
+    c("North America", "Latin America")
+  )
 
 data_bien_same_size <-
   data_bien %>%
-  dplyr::filter(
-    plot_area_ha == bien_most_common_size
+  tidyr::drop_na(longitude, latitude, plot_area_ha) %>%
+  dplyr::mutate(
+    continent = dplyr::case_when(
+      .default = "Other",
+      latitude > 18 & longitude < -40 ~ "North America",
+      latitude < 18 & longitude < -40 ~ "Latin America"
+    )
   ) %>%
-  tidyr::drop_na(longitude, latitude)
+  dplyr::filter(
+    continent %in% c("North America", "Latin America")
+  ) %>%
+  dplyr::mutate(
+    is_best = dplyr::case_when(
+      plot_area_ha == bien_most_common_size["North America"] &
+        continent == "North America" ~ TRUE,
+      plot_area_ha == bien_most_common_size["Latin America"] &
+        continent == "Latin America" ~ TRUE,
+      .default = FALSE
+    )
+  ) %>%
+  dplyr::filter(is_best)
 
 data_bien_same_size %>%
   ggplot2::ggplot(
@@ -102,7 +150,13 @@ data_bien_same_size %>%
     xlim = range(data_bien_same_size$longitude),
     ylim = range(data_bien_same_size$latitude)
   ) +
+  ggplot2::theme(
+    text = ggplot2::element_text(
+      size = 25
+    )
+  ) +
   ggplot2::labs(
+    title = "BIEN plots with most common plot size",
     x = "Longitude",
     y = "Latitude",
     caption = paste(
